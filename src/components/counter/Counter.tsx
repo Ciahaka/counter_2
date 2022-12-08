3b52cf591d15fbb766f92948d1bde3c214c56ba3
Ciahaka <wilddiver.by@gmail.com>
2022-12-08 15:48:44 +0300

    fix Counter for s

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import {SuperButton} from '../super button/SuperButton';
+import s from './Counter.module.css'
 
 export type CounterPropsType = {}
 
 export const Counter = (props: CounterPropsType) => {
 
   return (
-    <div>
-      <SuperButton name={'inc'}
-                   callback={() => {}}
-                   disabled={false}/>
-      <SuperButton name={'reset'}
-                   callback={() => {}}
-                   disabled={false}/>
+    <div className={s.count}>
+      <div className={s.number}>
+        <span>'''</span>
+      </div>
+      <div className={s.buttons}>
+        <SuperButton name={'inc'}
+                     callback={() => {}}
+                     disabled={false}/>
+
+        <SuperButton name={'reset'}
+                     callback={() => {}}
+                     disabled={false}/>
+      </div>
+
     </div>
   );
 };
