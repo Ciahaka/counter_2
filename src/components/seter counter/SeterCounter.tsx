1aed0321f2ccfb1a25c55c0b6aaf215f1089e41d
Ciahaka <wilddiver.by@gmail.com>
2022-12-08 15:48:56 +0300

    fix SetCounter for s

--- src/components/seter counter/SeterCounter.tsx
+++ src/components/seter counter/SeterCounter.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {SuperButton} from '../super button/SuperButton';
+import s from '../counter/Counter.module.css'
 
 export type SeterPropsType={
 
@@ -7,8 +8,16 @@ export type SeterPropsType={
 
 export const SeterCounter = (props:SeterPropsType) => {
   return (
-    <div>
-      <SuperButton name={'set'} callback={()=>{}} disabled={false}/>
+    <div className={s.count}>
+      <div className={`${s.number} ${s.input}`}>
+        <input type={'number'}/>
+        <input type={'number'}/>
+      </div>
+
+      <div className={s.buttons}>
+        <SuperButton name={'set'} callback={()=>{}} disabled={false}/>
+      </div>
+
     </div>
   );
 };
