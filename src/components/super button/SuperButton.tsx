175c123fff41982df3bcf69f8e022471def99476
Ciahaka <wilddiver.by@gmail.com>
2022-12-08 14:46:56 +0300

    add SuperButton component

---
+++ src/components/super button/SuperButton.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export type SuperButtonPops={
+  name:string
+  callback:()=>void
+  disabled:boolean
+}
+
+export const SuperButton = (props:SuperButtonPops) => {
+  const {name, callback, disabled}= props
+  const buttonClickHandler = ()=>{
+    callback()
+  }
+  return (
+    <button onClick={buttonClickHandler} disabled={disabled} >{name}</button>
+  );
+};
+
