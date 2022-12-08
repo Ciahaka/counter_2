fab3d82e2e51f569e75f02345f820b9395ba7c0d
Ciahaka <wilddiver.by@gmail.com>
2022-12-09 00:34:08 +0300

    ready Counter.tsx

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -2,23 +2,33 @@ import React from 'react';
 import {SuperButton} from '../super button/SuperButton';
 import s from './Counter.module.css'
 
-export type CounterPropsType = {}
+export type CounterPropsType = {
+  number: number
+  maxValue: number
+  startValue: number
+  disabled: boolean
+  incHandler: () => void
+  resetHandler: () => void
+}
 
 export const Counter = (props: CounterPropsType) => {
+ const {number, maxValue, startValue, disabled, incHandler, resetHandler} = props
 
   return (
     <div className={s.count}>
       <div className={s.number}>
-        <span>'''</span>
+        <span className={number === maxValue ? s.numberRed : ''}> {number}</span>
       </div>
       <div className={s.buttons}>
-        <SuperButton name={'inc'}
-                     callback={() => {}}
-                     disabled={false}/>
+        <SuperButton name="inc"
+                     callback={incHandler}
+                     disabled={number === maxValue}
+        />
 
-        <SuperButton name={'reset'}
-                     callback={() => {}}
-                     disabled={false}/>
+        <SuperButton name="reset"
+                     callback={resetHandler}
+                     disabled={number === startValue}
+        />
       </div>
 
     </div>
